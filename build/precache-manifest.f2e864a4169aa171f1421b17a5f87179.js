3a376a6b2615d2ea042064b80eb4618490bf9380
Matheo93 <Math.be2015@gmail.com>
2024-07-09 02:42:06 +0200

    Fix build issues and update dependencies

---
+++ build/precache-manifest.f2e864a4169aa171f1421b17a5f87179.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "337721d6177ac5f2981c7492cd8978d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fea415c6617906edc168",
+    "url": "/static/css/main.3447229e.chunk.css"
+  },
+  {
+    "revision": "b0c46b56f8a1e97584d3",
+    "url": "/static/js/2.76a3145b.chunk.js"
+  },
+  {
+    "revision": "e2a960147ef5362ced8a",
+    "url": "/static/js/3.1b3c587a.chunk.js"
+  },
+  {
+    "revision": "fea415c6617906edc168",
+    "url": "/static/js/main.5900ff02.chunk.js"
+  },
+  {
+    "revision": "e67b41604717847bfcd7",
+    "url": "/static/js/runtime~main.e0c0d350.js"
+  }
+]);
